14f3e4a552a605d82a6bccaefe6ffb3c80b1a15b
Atmena <tireau.steph@gmail.com>
2023-10-05 12:08:47 +0200

    chore: Initialisation de l'authentification JWT

--- src/index.php
+++ src/index.php
@@ -1,5 +1,28 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-phpinfo();
+require __DIR__ . '/vendor/autoload.php';
 
+$app = AppFactory::create();
+
+$jwtMiddleware = function ($request, $handler) {
+    $token = $request->getHeader('Authorization')[0] ?? null;
+    if (!$token) {
+        return new \Slim\Psr7\Response(401);
+    }
+
+    try {
+        $decoded = \Firebase\JWT\JWT::decode($token, 'your_secret_key', ['HS256']);
+        $request = $request->withAttribute('user', $decoded);
+        return $handler->handle($request);
+    } catch (\Exception $e) {
+        return new \Slim\Psr7\Response(401);
+    }
+};
+
+// Ajoutez vos routes ici
+
+$app->run();
 ?>
